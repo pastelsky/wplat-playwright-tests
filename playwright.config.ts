c45fac116c63aa769e11a8cd39a154aeb953567d
Shubham Kanodia <skanodia@atlassian.com>
2023-02-06 02:48:40 +0530

    Threshold 0

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,6 +20,9 @@ const config: PlaywrightTestConfig = {
          * Maximum time expect() should wait for the condition to be met.
          * For example in `await expect(locator).toHaveText();`
          */
+        toHaveScreenshot: {
+            threshold: 0,
+        },
         timeout: 5000
     },
     /* Run tests in files in parallel */
